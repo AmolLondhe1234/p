33646887f502e0cb63fe41960a7bd29c0a62c788
Amol Londhe <122874666+AmolLondhe1234@users.noreply.github.com>
2023-01-23 11:33:08 +0530

    Add files via upload

---
+++ portfolio/PF/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField()
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class certificate(models.Model):
+    cert_name=models.CharField( max_length=100)
+    cert_course=models.CharField(max_length=100)
+    cert_institute=models.CharField(max_length=100)
+    cert_img=models.ImageField( upload_to=None, height_field=None, width_field=None, max_length=None)
+    
+    def __str__(self):
+        return self.cert_name
+    
+    
+class feedback(models.Model):
+    fname=models.CharField(max_length=100)
+    email=models.EmailField()
+    phone=models.IntegerField()
+    feedback_text=models.TextField()
+    
+    def __str__(self):
+        return self.fname
+        
+    
---
+++ portfolio/PF/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from .models import certificate,Project,feedback
+# Create your views here.
+
+from django.shortcuts import HttpResponse
+
+def profile(request):
+    # return HttpResponse("Hello World")
+    
+    return render(request,'home.html')
+
+def ps(request):
+    return render(request,'ps.html')
+
+def skill(request):
+    
+    return render(request,'home.html')
+
+
+def cert(request):
+    cert=certificate.objects.all()
+    context={
+        cert:'cert'
+    }
+    return render(request,'certificates.html',context)
+
+def hireme(request):
+    return render(request,'hireme.html')
+
+def project(request):
+    projects = Project.objects.all()
+    return render(request, 'project.html', {'projects': projects})
+
+
+def feedback(request):
+    return render(request,'feedback.html')
