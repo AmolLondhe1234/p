c5aef666b9dbb654f1f668c1a331e0cb0aa6989c
zaheer1213 <zaheerbaig242@gmail.com>
2023-01-23 18:07:48 +0530

    modifications

--- portfolio/PF/models.py
+++ portfolio/PF/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to='images/')
     url = models.URLField(blank=True)
 
     def __str__(self):
