d5074d531388c8d7c419ef410567a062ccfa9f1b
zaheer1213 <amolgithub123@gmail.com>
2023-01-25 22:17:16 +0530

    done hosting settings

--- portfolio/bulid_files.sh
+++ portfolio/bulid_files.sh
@@ -27,6 +27,6 @@
 #   ],
 # }
 echo "BUILD START"
-python3.10 -m pip install -r requirements.txt
+python3.10 -m pip3 install -r requirements.txt
 python3.10 manage.py collectstatic --noinput --clear
 echo "BUILD END" 
